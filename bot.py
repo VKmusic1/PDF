0cbfc946f2d897d89ee2951b5e25a24760868df9
VKmusic1 <nalog566766@gmail.com>
2025-06-05 18:23:25 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import asyncio
 import fitz  # PyMuPDF
 import pdfplumber
 import pandas as pd
+import openpyxl
 from flask import Flask, request
 from telegram import Update, InputFile, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
